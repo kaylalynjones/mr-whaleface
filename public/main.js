721a16857c5b877f1f365a89a11fab25d9bc6a5e
Kayla Jones <kaylalyne424@gmail.com>
2014-10-27 10:49:26 -0500

    working on menu screen

--- public/main.js
+++ public/main.js
@@ -1,12 +1,29 @@
-var game = new Phaser.Game(550, 685, Phaser.AUTO, 'gameboard'),
-    hole,
-    i;
+var game = new Phaser.Game(550, 685, Phaser.AUTO, 'gameboard');
+
+var start = {
+  preload: function(){
+    game.load.image('bkgrd', 'assets/bkgrd.png');
+    game.load.image('character', 'assets/whale-lrg.png');
+    game.load.image('shell', 'assets/shell.png');
+  },
+  create: function(){
+    this.background = this.game.add.sprite(0, 0, 'bkgrd');
+    this.character = this.game.add.sprite(90, 200, 'character');
+    this.character.scale.x = 0.7;
+    this.character.scale.y = 0.7;
+    this.shell = this.game.add.sprite(130, 400, 'shell');
+    this.title = game.add.text(50, 30, 'Mr. Whaleface', {font: '70px Arial', fill: '#ffffff'});
+  },
+  update: function(){
+
+  }
+};
+//*****************************************************************
 var mainState = {
   preload: function(){
     game.stage.backgroundColor = '#71c5cf';
     game.load.image('bkgrd', 'assets/bkgrd.png');
-    //game.load.spritesheet('whale', 'assets/whale.png', 193, 100, 3);
-    game.load.image('whale', 'assets/whale.png');
+    game.load.spritesheet('whale', 'assets/whale-sprite.png', 193, 100);
     game.load.image('rock','assets/rock.png');
     game.load.image('anchor', 'assets/anchor.png');
     game.load.audio('splat', 'assets/jab.mp3');
@@ -34,16 +51,16 @@ var mainState = {
     });
 
     this.whale = this.game.add.sprite(100, 245, 'whale');
-    //this.whale.animations.add('swim');
-    //this.whale.animations.play('swim', 30, true);
-
+    this.whale.animations.add('swim', [1,2,3], 5, true);
     game.physics.arcade.enable(this.whale);
     this.whale.body.gravity.y = 1000;
-
+    this.whale.scale.x = 0.5;
+    this.whale.scale.y = 0.5;
     var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
     this.whale.anchor.setTo(-0.2, 0.5);
-    this.whale.body.height = 20;
+    this.whale.body.height = 5;
+
     this.score = -1;
     this.labelScore = game.add.text(40, 20, '0', {font: '30px Arial', fill: '#ffffff'});
     this.splatSound = game.add.audio('splat');
@@ -57,14 +74,15 @@ var mainState = {
     game.physics.arcade.overlap(this.whale, this.rocks, this.hitObstacle, null, this);
     game.physics.arcade.overlap(this.whale, this.anchors, this.hitObstacle, null, this);
 
-    if(this.whale.angle < 7)
+    if(this.whale.angle < 5)
       this.whale.angle += 1;
+    this.whale.animations.play('swim');
   },
   jump: function(){
     if(this.whale.alive === false)
       return;
     this.whale.body.velocity.y = -350;
-    game.add.tween(this.whale).to({angle: -5}, 100).start();
+    game.add.tween(this.whale).to({angle: -2}, 150).start();
     this.jumpSound.play();
   },
   restartGame: function(){
@@ -90,7 +108,7 @@ var mainState = {
     this.score += 1;
     this.labelScore.text = this.score;
 
-    this.addAnchor(500, height - 90);
+    this.addAnchor(500, height - 125);
   },
   hitObstacle: function(){
     if(this.whale.alive === false)
@@ -120,5 +138,6 @@ var mainState = {
   }*/
 };
 
+game.state.add('start', start);
 game.state.add('main', mainState);
-game.state.start('main');
+game.state.start('start');
